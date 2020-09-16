084e458628e96ef43e31a76355dec189a88306b9
juniormarangao <70915283+juniormarangao@users.noreply.github.com>
2020-09-15 23:15:22 -0300

    Update dirremoto.sh

--- dirremoto.sh
+++ dirremoto.sh
@@ -3,35 +3,57 @@
 # Script para facilitar acesso à diretórios remotos na rede
 #
 # Versão 0.1
-# Setembro 06, 2020
+# Versão 0.2 - 	Adicionando teste para verificar se ponto de montagem
+#				contém "/" ao final e removendo caso tenha.
+#				Adicionado opção de inserir número de porta.
+#				Setembro 15, 2020
 #
-# Criado por Marangão
+# Criado por Marangão - Setembro 06, 2020
 # marangao@vmcloud.ga
 #
 # Licença GPL
 #
 #
-# Script para auxiliar/facilitar a montagem de diretório/pasta
-# remoto em seu sistema.
+# Script para auxiliar/facilitar a montagem de diretório/pasta remoto em seu sistema.
 # Exemplo de uso:
 #
-# Execute esse script e o mesmo irá solicitar o server onde você
-# pode entrar como IP/diretorio ou hostname/diretorio
-# 
-# $ 192.168.1.10/musica
+# Execute esse script e o mesmo irá solicitar o server onde você pode entrar como IP/diretorio ou hostname/diretorio
+# 192.168.1.10/musica
 # 
 # O programa também solicitará nome de usuário e senha, também como ponto de montagem
 # Caso seu ponto de montagem não exista será questionado se quer criá-lo.
-# Se o o ponto de montagem já existir, com autopreenchimento fica
-# "/mnt/musicas/", mas para um melhor funcionamento usar "/mnt/musica"
-#
 
-######### Teste se é ROOT ###########
+### Testando se é ROOT ###
 if [[ $(id -u) -ne 0 ]]; then
 	echo -e "\nSOMENTE ROOT\n"
 	exit 1
 fi
-#####################################
+
+#########################################################################
+######### ERRO NO TRECHO / ESTUDAR MAIS #########
+#readonly DEPENDENCIES=(cifs)
+
+#checkDependencies() {
+#	local errorFound=0
+#
+#	for command in "${DEPENDENCIES[@]}"; do
+#		if ! which "$command" > /dev/null; then
+#			echo -e "\nERRO: comando não encontrado '$command'" >&2
+#			errorFound=1
+#		fi
+#	done
+#
+#	if [[ "$errorFound" -ne "0" ]]; then
+#		echo -e "\n----- IMPOSSÍVEL CONTINUAR -----"
+#		echo "Esse script precisa dos comandos listados acima" >&2
+#		echo "Instale-os e/ou verifique se estão no seu \$PATH" >&2
+#		exit 1
+#	fi
+#}
+#
+#
+#	checkDependencies
+#########################################################################
 
 read -p "Entre com o diretorio remoto: " remoteDir # Não precisa usar "//" no início
 read -p "Informe o usuário: " remoteUser
@@ -39,30 +61,37 @@ echo "Informe a senha: "
 read -s remotePass
 echo "Entre com o ponto de montagem: " # Diretório onde quer montar
 read -e mountDir
+read -p "Informe a porta, se houver: " port
 
-######### VARIÁVEIS PARA USAR EM TESTES #########
+[[ "$port" ]] || port="445" # Se não inserir porta, define como padrão 445
+
+######### VERIFICA SEM TEM "/" NO FINAL E REMOVENDO SE TIVER #########
+echo "$mountDir" > pontodemontagem.txt # CRIA TEMPORARIAMENTE ARQUIVO
+if [[ "$(grep -o '.$' pontodemontagem.txt)" = "/" ]]; then
+	mountDir=$(sed 's/.$//g' pontodemontagem.txt)
+fi
+rm pontodemontagem.txt # REMOVE ARQUIVO CRIADO TEMPORARIAMENTE
+
 mountedDir=$(df -h | grep "$mountDir" | cut -d '%' -f2 | cut -d ' ' -f2)
 mounted=$(df -h | grep "$mountDir")
 
 #test "$mountDir" = "$mountedDir" \
 #	&& { echo -e "\nJá está montado\n$mounted\n" ; exit 0 ;}
 
-######### TESTE SE DIRETÓRIO JÁ ESTÁ MONTADO #########
 if [[ "$mountDir" = "$mountedDir" ]]; then
-	echo -e "\nJá está montado\n"$mounted"\n"
+	echo -e "\nJá está montado\n$mounted\n"
 	exit 0
 fi
 
-######### TESTE SE PONTO DE MONTAGEM EXISTE OU NÃO #########
-if [ -e "$mountDir" ]; then
+if [ -e $mountDir ]; then
 	echo -e "\nMontando diretório remoto"
-	mount -t cifs -o username="$remoteUser",password="$remotePass" //"$remoteDir" "$mountDir"
-	echo -e "Montado com sucesso: $(df -h | grep "$mountDir") \n"
+	mount -t cifs -o username="$remoteUser",password="$remotePass",port="$port" //"$remoteDir" "$mountDir"
+	echo -e "Montado com sucesso: $(df -h | grep $mountDir) \n"
 	exit 0
 else
 	read -p "Diretório não existe. Deseja criar? [S/N]: " confirm
 	case "$confirm" in
-	# CONFERENCIA DE RESPOSTA DO USUÁRIO #
+
 	s|S|"")
 		mkdir "$mountDir"
 	;;
@@ -78,6 +107,5 @@ else
 fi
 
 echo -e "\nMontando diretório remoto..."
-mount -t cifs -o username="$remoteUser",password="$remotePass" //"$remoteDir" "$mountDir"
+mount -t cifs -o username="$remoteUser",password="$remotePass",port="$port" //"$remoteDir" "$mountDir"
 echo -e "Montado com sucesso: $(df -h | grep "$mountDir") \n"
-
