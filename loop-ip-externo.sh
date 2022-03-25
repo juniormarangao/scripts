fdd09a0c191ae681d1ddc81a2f932341321a32c0
junior <junior@arch>
2022-03-25 08:59:02 -0300

    Substituição de servidor e melhorias no codigo

--- loop-ip-externo.sh
+++ loop-ip-externo.sh
@@ -2,47 +2,48 @@
 #
 # Script fecha looping e fica buscando IPv4 público e manda para telegram
 # via telegram-notify, caso tenha configurado.
+# Para internet com IP diâmico.
 # Forma de uso: ./loop-ip-externo.sh &
 # Assim roda em background
 # Uma boa opção é por para rodar no boot
-#
-######## VARIAVEIS ######################
-					#
-servidor1="ifconfig.co"			#
-servidor2="icanhazip.com"		#
-servidor3="ifconfig.me"			#
-					#
-#########################################
 
-buscaIP=$(curl -s ifconfig.me)
+LOG="/tmp/loop-ip.log"
+# Checar dependencias.
+readonly dependencias=(curl telegram-notify wget)
+checarDependencias() {
+	local erro=0
+	for comando in "${dependencias[@]}"; do
+		if ! which "$comando" 2>1 > /dev/null ; then
+			echo -e "\nComando $comando não encontrado" >&2
+			local erro=1
+		fi
+	done
+	if [[ "$erro" != "0" ]]; then
+		echo -e "\nErro. Verifique se os comandos listados estão instalados ou em $PATH\n" >&2
+		exit 1
+	fi
+}
+
+checarDependencias # Chamada da função
+
+servidor="https://marantech.com.br/getip.php"
 armazenaIP=""
-
-#if wget -q --spider www.google.com; then
-#	echo -e "\nEstá com internet!"
-#	echo -e "Seu IP público é: $armazenaIP\n"
-#	exit 0
-#else
-#	echo -e "\nPrecisa estar conectado à internet.\n"
-#	exit 1
-#fi
-
-while [[ "1" -gt "0" ]]; do
-	if wget -q --spider "$servidor1"; then
-		buscaIP4=$(curl -s -4 "$servidor1")
-		buscaIP6=$(curl -s -6 "$servidor1")
-	elif wget -q --spider "$servidor2"; then
-		sleep 2s
-		buscaIP4=$(curl -s -4 "$servidor2")
-		buscaIP6=$(curl -s -6 "$servidor2")
+while : ; do
+	if wget -q --spider "$servidor" 2> /dev/null; then
+		buscaIP4=$(curl -s -4 "$servidor" | head -n1)
+		buscaIP6=$(curl -s -6 "$servidor" | head -n1)
 	else
-		buscaIP4=$(curl -s -4 "$servidor3")
-		buscaIP6=$(curl -s -6 "$servidor3")
+		echo "Sem internet"
 	fi
 	if [[ "$armazenaIP" != "$buscaIP4" ]]; then
 		armazenaIP="$buscaIP4"
-		echo "
-		Seu IP Público mudou, agora é: $armazenaIP
-		Seu IPv6 é: $buscaIP6" | ./telegram-notify --text -
+		echo "Seu IP Público mudou, agora é: $armazenaIP" > $LOG
+		if [[ -z "$buscaIP6" ]]; then
+			echo "Sem IPv6" >> $LOG
+		else
+			echo "Seu IPv6 é: $buscaIP6" >> $LOG
+		fi
+		cat "$LOG" | telegram-notify --text -
 	fi
 	sleep 5s
 done
