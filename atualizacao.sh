f0f50de404bd729ad938626fb11bb2c4825eb95f
junior <junior@arch>
2023-04-02 20:30:25 -0300

    Lines from sensors and telegram notify commented

--- atualizacao.sh
+++ atualizacao.sh
@@ -2,9 +2,9 @@
 #
 # Executa os comandos de atualizacao do sistema e envia arquvi de LOG para o Telegram
 #
-#Versão 0.2: Adicionando suporte à opção de help (-h)
-#Versao 0.3: Adicionado informçao de temperatura, execuçao se somente root e alteraçoes de log, 4 Setembro 2020
-#
+# Versão 0.2: Adicionando suporte à opção de help (-h)
+# Versao 0.3: Adicionado informçao de temperatura, execuçao se somente root e alteraçoes de log, 4 Setembro 2020
+# Versão 0.4: Linhas comentadas de sensor e de envio para telegram, necessita adiçao manual.
 #
 #Criado por Marangão, Maio de 2019
 #
@@ -46,44 +46,44 @@ echo "$dia Atualizacao diaria iniciada
 
 " > $LOG
 
-apt-get update >> $LOG
+apt-get update >> $LOG 2>&1
 
 echo "
 
 UPGRADE
 
-" >> $LOG
+" >> $LOG 2>&1
 
-apt-get upgrade -y >> $LOG
+echo -e "\n" | apt-get upgrade -y >> $LOG 2>&1
 
 echo "
 
 Dist Upgrade
 
-" >> $LOG
+" >> $LOG 2>&1
 
-apt-get dist-upgrade -y >> $LOG
+echo -e "\n" | apt-get dist-upgrade -y >> $LOG 2>&1
 
 echo "
 
 Limpeza com Autoclean e Clean
 
-" >> $LOG
+" >> $LOG 2>&1
 
-apt-get autoclean >> $LOG
+apt-get autoclean >> $LOG 2>&1
 apt-get clean
 
 # PEGA TEMPERATURA DOS CORES DO PROCESSADOR
 # Neste caso abaixo está pegando apenas de um processador
 # com 2 cores, caso seu PC tenha mais, adicione mais linhas
 # como as seguintes
-tempCore1=$(sensors | grep Core | awk -F: '{print $2}' | cut -d '+' -f2 | cut -d ' ' -f1 | sed -n '1p')
-tempCore2=$(sensors | grep Core | awk -F: '{print $2}' | cut -d '+' -f2 | cut -d ' ' -f1 | sed -n '2p')
+#tempCore1=$(sensors | grep Core | awk -F: '{print $2}' | cut -d '+' -f2 | cut -d ' ' -f1 | sed -n '1p')
+#tempCore2=$(sensors | grep Core | awk -F: '{print $2}' | cut -d '+' -f2 | cut -d ' ' -f1 | sed -n '2p')
 #############################################################
 
-echo "
-A temperatura do Core 1 é $tempCore1 e do Core 2 $tempCore2
+#echo "
+#A temperatura do Core 1 é $tempCore1 e do Core 2 $tempCore2
 
-Atualização provavelmente concluída, pronto para envio de log para Telegram" >> $LOG
+#Atualização provavelmente concluída, pronto para envio de log para Telegram" >> $LOG
 
-./telegram-notify --document $LOG
+#./telegram-notify --document $LOG
